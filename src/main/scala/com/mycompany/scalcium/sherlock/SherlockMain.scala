d5e3aa295a8a3307d8bc8d86a96238eda395e88d
Pal <palsujit@ELSSDGL-6001969.science.regn.net>
2015-07-25 18:01:28 -0700

    Adding computation for average #-words/structure.

--- src/main/scala/com/mycompany/scalcium/sherlock/SherlockMain.scala
+++ src/main/scala/com/mycompany/scalcium/sherlock/SherlockMain.scala
@@ -5,6 +5,7 @@ import java.io.FileWriter
 import java.io.PrintWriter
 
 import scala.Array.canBuildFrom
+import scala.collection.mutable.ArrayBuffer
 
 import org.apache.commons.io.FileUtils
 
@@ -17,24 +18,58 @@ object SherlockMain extends App {
     val sentenceSplitter = new SentenceSplitter()
     val nerFinder = new NERFinder()
     
-    val outfile = new PrintWriter(
-        new FileWriter(new File(dataDir, "output.tsv")), true)
+    preprocess()
+    wordCounts()
     
-    val gutenbergFiles = new File(dataDir, "gutenberg").listFiles()
-    gutenbergFiles.zipWithIndex.map(fi => {
-            val text = FileUtils.readFileToString(fi._1, "UTF-8")
-            (fi._2, text)
-        })
-        .map(ft => (ft._1, metadataRemover.removeMetadata(ft._2)))
-        .flatMap(ft => paragraphSplitter.split(ft))
-        .flatMap(fpt => sentenceSplitter.split(fpt, false))
-        .flatMap(fpst => nerFinder.find(fpst))
-        .foreach(fpstt => save(outfile, fpstt.productIterator))
-
-    outfile.flush()
-    outfile.close()
+    
+    def preprocess(): Unit = {
+        val outfile = new PrintWriter(
+            new FileWriter(new File(dataDir, "output.tsv")), true)
+        val gutenbergFiles = new File(dataDir, "gutenberg").listFiles()
+        gutenbergFiles.zipWithIndex.map(tf => {
+                val text = FileUtils.readFileToString(tf._1, "UTF-8")
+                (tf._2, text)
+            })
+            .map(ft => (ft._1, metadataRemover.removeMetadata(ft._2)))
+            .flatMap(ft => paragraphSplitter.split(ft))
+            .flatMap(fpt => sentenceSplitter.split(fpt, false))
+            .flatMap(fpst => nerFinder.find(fpst))
+            .foreach(fpstt => save(outfile, fpstt.productIterator))
+        outfile.flush()
+        outfile.close()
+    }
         
     def save(outfile: PrintWriter, line: Iterator[_]): Unit = {
         outfile.println(line.toList.mkString("\t"))
+    }
+
+    def wordCounts(): Unit = {
+        val gutenbergFiles = new File(dataDir, "gutenberg").listFiles()
+        val numWordsInFile = ArrayBuffer[Int]()
+        val numWordsInPara = ArrayBuffer[Int]()
+        val numWordsInSent = ArrayBuffer[Int]()
+        gutenbergFiles.zipWithIndex.foreach(tf => {
+            val text = metadataRemover.removeMetadata(
+                FileUtils.readFileToString(tf._1, "UTF-8"))
+            numWordsInFile += text.split(" ").size
+            val paras = paragraphSplitter.split((tf._2, text))
+            paras.foreach(para => {
+                numWordsInPara += para._3.split(" ").size
+                val sents = sentenceSplitter.split(para, false)
+                sents.foreach(sent => 
+                    numWordsInSent += sent._4.split(" ").size)
+            })
+        })
+        Console.println("Average words/file: %.3f".format(
+            mean(numWordsInFile.toArray)))                
+        Console.println("Average words/para: %.3f".format(
+            mean(numWordsInPara.toArray)))
+        Console.println("Average words/sent: %.3f".format(
+            mean(numWordsInSent.toArray)))
+    }
+    
+    def mean(xs: Array[Int]): Double = {
+        val sum = xs.foldLeft(0)(_ + _)
+        sum.toDouble / xs.size
     }
 }
